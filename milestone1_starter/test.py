5907fe9e623ae8e6a3752645d05ff77944ab705d
nmasharani <nisham@>
2013-04-14 21:34:08 -0700

    bits to image compelte

--- milestone1_starter/test.py
+++ milestone1_starter/test.py
@@ -2,12 +2,11 @@ from source import Source
 from sink import Sink
 
 
-src = Source(0, "testfiles/32pix.png")
+src = Source(0, "testfiles/checkerboard.png")
 
 src_payload, src_databits = src.process()
 
 sink = Sink()
-
 
 recd_bits = sink.process(src_databits)
 
